04f648ac4b5224b1a6419d7357706ffb824b818d
Derek Everett <derekeverett@Dereks-2024-MacBook-Pro.local>
2025-05-09 08:59:29 -0400

    This commit adds inline documentation to the train_model.py script. It also changes the name of the IrisRequest class to just Request, which is more general if we change the underlying ML model in the future.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pydantic.types import conlist
-# from typing import Annotated
 import joblib
 import numpy as np
 
-class IrisRequest(BaseModel):
+class Request(BaseModel):
     features: conlist(float, min_length=4, max_length=4)
 
 app = FastAPI()
 model = joblib.load("model.joblib")
 
 @app.post("/predict")
-def predict(req: IrisRequest):
+def predict(req: Request):
     prediction = model.predict(np.array([req.features]))
     return {"prediction": int(prediction[0])}
--- train_model.py
+++ train_model.py
@@ -2,7 +2,10 @@ import joblib
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
+# load the iris dataset
 X, y = load_iris(return_X_y=True)
+# fit a Random Forest Model
 model = RandomForestClassifier()
 model.fit(X, y)
+# serialize the model and save to disk
 joblib.dump(model, "model.joblib")
